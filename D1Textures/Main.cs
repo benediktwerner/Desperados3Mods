038b02d9d14d524a641be03f976b87523f14b44f
Benedikt Werner <1benediktwerner@gmail.com>
2022-09-07 07:54:27 +0200

    D1 Textures

---
+++ D1Textures/Main.cs
@@ -0,0 +1,90 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using System.IO;
+using UnityEngine;
+
+namespace Desperados3Mods.D1Textures
+{
+    [BepInPlugin(GUID, Name, Version)]
+    public class Main : BaseUnityPlugin
+    {
+        public const string GUID = "de.benediktwerner.desperados3.d1textures";
+        public const string Name = "D1 Textures";
+        public const string Version = "1.0.0";
+
+        static ConfigEntry<bool> Enabled;
+
+        void Awake()
+        {
+            Enabled = Config.Bind("General", "Enabled", true);
+
+            Harmony.CreateAndPatchAll(typeof(Hooks));
+        }
+
+        static class Hooks
+        {
+            static float lastReset = 0f;
+
+            [HarmonyPatch(typeof(MiSaveable), "finishedLoading")]
+            [HarmonyPostfix]
+            public static void MiSaveable_finishedLoading()
+            {
+                if (!Enabled.Value || Time.time - lastReset < 2f) return;
+                lastReset = Time.time;
+
+                Debug.Log("Changing textures");
+
+                var gameInput = MiGameInput.instance;
+                if (gameInput == null) return;
+
+                Debug.Log("Have game input");
+
+                foreach (var c in gameInput.lPlayableCharacter)
+                {
+                    if (c.m_eCharacter == MiCharacter.CharacterType.Cooper)
+                    {
+                        var visRoot = FindStartsWith(c.transform, "MiVis")?.Find("ply_gnc_cooper_00");
+                        if (visRoot == null) continue;
+
+                        AssignTexture("coop", visRoot.Find("ply_gnc_cooper_00"), visRoot.Find("ply_gnc_cooper_hat_00"));
+                        AssignTexture("coop_coat", visRoot.Find("ply_gnc_cooper_coat_00"));
+                    }
+                    else if (c.m_eCharacter == MiCharacter.CharacterType.Kate)
+                    {
+                        var visRoot = FindStartsWith(c.transform, "MiVis")?.Find("ply_gnc_kate_00");
+                        if (visRoot == null) continue;
+
+                        AssignTexture("kat", visRoot.Find("ply_gnc_kate_00"));
+                        AssignTexture("kat_cloth", visRoot.Find("ply_gnc_kate_cloth_00"));
+                        AssignTexture("kat_hat", visRoot.Find("ply_gnc_kate_hat_00"));
+                    }
+                }
+            }
+
+            static Transform FindStartsWith(Transform t, string n)
+            {
+                var count = t.childCount;
+                for (var i = 0; i < count; i++)
+                {
+                    var child = t.GetChild(i);
+                    if (child.name.StartsWith(n)) return child;
+                }
+                return null;
+            }
+
+            static void AssignTexture(string name, params Transform[] ts)
+            {
+                string path = $"Desperados III_Data/Resources/Textures/{name}.png";
+                Texture2D texture = new Texture2D(2, 2);
+                ImageConversion.LoadImage(texture, File.ReadAllBytes(path));
+                foreach (var t in ts)
+                {
+                    var renderer = t?.GetComponent<SkinnedMeshRenderer>();
+                    if (renderer == null) continue;
+                    renderer.material.mainTexture = texture;
+                }
+            }
+        }
+    }
+}
--- create_release.py
+++ create_release.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
 import os, zipfile, shutil
+from typing import List, Tuple
 
 mods = [
     "BetterLevelEditor",
     "Convenience",
     "D1CooperGun",
+    "D1Textures",
     "DevKillsList",
     "ExtendedCheats",
     "KingsmanEasterEgg",
@@ -16,21 +18,38 @@ RELEASES_DIR = "releases"
 
 os.makedirs("releases", exist_ok=True)
 
-for mod in mods:
-    dll = os.path.join(mod, "bin", "Release" , mod + ".dll")
-    if mod == "BetterLevelEditor":
-        files = []
-        files.append((dll, mod + ".dll"))
-        files.append((os.path.join(mod, "spawn_codes.txt"), "spawn_codes.txt"))
 
-        for f, _ in files:
-            if not os.path.isfile(f):
-                print(f"{mod}: {f} not found. Skipping")
-                break
-        else:
-            outfile = os.path.join(RELEASES_DIR, mod + ".zip")
-            with zipfile.ZipFile(outfile, "w", zipfile.ZIP_DEFLATED) as zipf:
-                for f, n in files:
-                    zipf.write(f, n)
+def create_zip(mod: str, files: List[Tuple[str, str]]) -> None:
+    for f, _ in files:
+        if not os.path.isfile(f):
+            print(f"{mod}: {f} not found. Skipping mod.")
+            break
+    else:
+        outfile = os.path.join(RELEASES_DIR, mod + ".zip")
+        with zipfile.ZipFile(outfile, "w", zipfile.ZIP_DEFLATED) as zipf:
+            for f, n in files:
+                zipf.write(f, n)
+
+
+for mod in mods:
+    dll = os.path.join(mod, "bin", "Release", mod + ".dll")
+    if mod == "BetterLevelEditor":
+        files = [
+            (dll, mod + ".dll"),
+            (os.path.join(mod, "spawn_codes.txt"), "spawn_codes.txt"),
+        ]
+        create_zip(mod, files)
+
+    elif mod == "D1Textures":
+        files = [(dll, f"BepInEx/plugins/{mod}.dll")]
+        for fname in os.listdir(f"{mod}/Textures"):
+            files.append(
+                (
+                    f"{mod}/Textures/{fname}",
+                    f"Desperados III_Data/Resources/Textures/{fname}",
+                )
+            )
+        create_zip(mod, files)
+
     else:
         shutil.copy(dll, RELEASES_DIR)
